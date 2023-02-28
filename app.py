b5e7c216e915cef08e0198c0aac8eb9b96d9fffb
Romagnhp <romagnhp@gmail.com>
2023-02-28 09:46:17 +0200

    flask

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import datetime
+
+obj_Flask = Flask(__name__)
+
+myDictionary = {'name':'Honcharov Roman', 'phoneNumber':'099-248-47-98', 'mail':'romagnhp@gmail.com'}
+
+def myStyle():
+    t = datetime.datetime.now().hour
+    if 6 <= t < 17:
+        return 'css/styleWhite.css'
+    else:
+        return 'css/styleBlack.css'
+
+@obj_Flask.route("/")
+def lounch_Rezume():
+    #return render_template("/index.html", name = 'Honcharov Roman', phoneNumber = '099-248-47-98', mail= 'romagnhp@gmail.com')
+    return render_template("/index.html", **myDictionary, back_ground_color = '#000', text_color = '#fff', filename = 'css/style.css', cssPath = myStyle())
+
+obj_Flask.run(debug=True) 
